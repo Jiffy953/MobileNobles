54aac05ea90ee23183e931ae659d9511a72fdf3c
“Adeline <jareds2147@gmail.com>
2023-04-24 21:18:55 -0400

    hid app title bar

--- app/src/main/java/com/poly/mobilenobles/MainActivity.kt
+++ app/src/main/java/com/poly/mobilenobles/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     private val chessMainLoop = ChessMainLoop()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
 
         // Create the ChessboardView instance and pass the ChessMainLoop instance to it
         val chessboardView = ChessboardView(this, null, chessMainLoop)
